78c10b96ee0cfb92056ee71ba3c0eafb70c0a389
aleks <aleks.mihaylov69@gmail.com>
2021-09-29 18:50:50 +0300

    add number validator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
+import React, { useState, useMemo } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+
+  const isNum = useMemo(() => {
+    const icon = document.querySelector(".fas");
+    if (icon === null) return;
+    if (isNaN(input) || input === " " || input === "") {
+      icon.classList.remove("fa-check");
+      icon.classList.add("fa-times");
+      return false;
+    } else {
+      icon.classList.remove("fa-times");
+      icon.classList.add("fa-check");
+      return true;
+    }
+  }, [input]);
   return (
     <div className="App">
-     <div className="control has-icons-right">
+      <div className="control has-icons-right">
         <input
           className="input is-large"
-          type="text"
+          type="input"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
           placeholder="Enter number..."
-
         />
         <span className="icon is-small is-right">
           <i className="fas fa-times" />
-        </span> 
+        </span>
       </div>
     </div>
   );
